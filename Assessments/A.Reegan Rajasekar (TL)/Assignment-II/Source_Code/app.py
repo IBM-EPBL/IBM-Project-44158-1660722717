bf5cba6b3b37de7824cf7d6f860e27543725e05b
reeganrajasekar <areeganrajasekar@gmail.com>
2022-09-25 19:30:08 +0530

    Assignment-II Source Code

--- Assessments/A.Reegan Rajasekar (TL)/Assignment-II/Source_Code/app.py
+++ Assessments/A.Reegan Rajasekar (TL)/Assignment-II/Source_Code/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template , request , redirect
 import ibm_db
-conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=9938aec0-8105-433e-8bf9-0fbb7e483086.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32459;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;PROTOCOL=TCPIP;UID=bbf86602;PWD=iAjizpHEzfYvlS6v", '', '')
+conn = ibm_db.connect("URL", '', '')
 
 app = Flask(__name__)
 
